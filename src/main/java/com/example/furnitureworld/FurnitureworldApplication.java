14d334f722c4c0f99178cfbc38ccfc3441c55e9a
Sahak-S <sahak.sa@mail.ru>
2022-03-19 22:29:07 +0400

    Initial commit

---
+++ src/main/java/com/example/furnitureworld/FurnitureworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.furnitureworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurnitureworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/furnitureworld/FurnitureworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.furnitureworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
